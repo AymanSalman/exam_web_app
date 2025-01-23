db06e61953ac75ea535eb47231ea110b094182ac
Ayman Salman <salman.ayman.87@gmail.com>
2025-01-23 17:34:10 +0200

    Fixed status view

--- status/views.py
+++ status/views.py
@@ -3,15 +3,10 @@ import os
 from datetime import datetime
 
 def status_view(request):
-    status_value = os.getenv('MY_WEB_APP_STATUS', None)
-    if status_value:
-        response = {
-            'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'status': status_value
-        }
-    else:
-        response = {
-            'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'status': 'MY_WEB_APP_STATUS is not set'
-        }
+    status_value = os.getenv('MY_WEB_APP_STATUS', 'MY_WEB_APP_STATUS is not set')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    response = {
+        "Current time now is": current_time,
+        "The status of the ENV MY_WEB_APP_STATUS": status_value
+    }
     return JsonResponse(response)
